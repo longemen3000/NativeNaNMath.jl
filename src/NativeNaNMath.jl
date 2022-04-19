253316f48368982ea9a9139f7ec1b5e997aad467
longemen3000 <longemen3000@gmail.com>
2022-04-19 17:33:47 -0400

    first commit

--- src/NativeNaNMath.jl
+++ src/NativeNaNMath.jl
@@ -1,5 +1,397 @@
 module NativeNaNMath
+using Base: IEEEFloat
 
-# Write your package code here.
+const BigNaN = big"NaN"
+
+@inline nan(::T) where T = nan(T)
+@inline nan(::Type{Float16}) = NaN16
+@inline nan(::Type{Float32}) = NaN32
+@inline nan(::Type{Float64}) = NaN64
+#TODO: define nan for integer types
+@inline nan(::Type{BigFloat}) = BigNaN
+@inline nan(::Type{BigInt}) = BigNaN
+@inline nan(::Type{T}) where T<:Real = zero(T)/zero(T) #catch all, works with AD without specific rules
+
+#functions with non-negative domain
+for f in (:log,:log2,:log10,
+        :sqrt)
+
+    @eval begin
+        $f(x) = Base.$f(x)
+        function $f(x::Real)
+            in_domain = x >= zero(x)
+            Base.$f(ifelse(in_domain,x,nan(x)))
+        end
+    end
+end
+
+#two-argument log. TODO: find a better version
+log(x,base) = Base.log(x,base)
+function log(x::Real,base::Real)
+    in_domain = (x >= zero(x)) & (base > zero(base)) 
+    Base.log(ifelse(in_domain,x,nan(x)),ifelse(in_domain,base,nan(base)))
+end
+
+#displaced 
+log1p(x) = Base.log1p(x)
+function log1p(x::Real)
+    in_domain = x >= -one(x)
+    Base.log1p(ifelse(in_domain,x,nan(x)))
+end
+
+#functions with finite domain:
+#the value is set to zero if not it domain
+for f in (:sin,:sind,:sinpi,
+        :cos,:cosd,:cospi,
+        :tan,:tand,
+        :sec,:secd,
+        :cot,:cotd,
+        :csc,:cscd)
+
+    @eval begin
+        $f(x) = Base.$f(x)
+        function $f(x::Real)
+            in_domain = !isinf(x)
+            Base.$f(ifelse(in_domain,x,nan(x)))
+        end
+    end
+end
+
+#two calls to nan(x) seems excessive. consider using native BigFloat call instead
+for f in (:sincos,:sincosd,:sincospi)
+    @eval begin
+        $f(x) = Base.$f(x)
+        function $f(x::Real)
+            in_domain = !isinf(x)
+            Base.$f(ifelse(in_domain,x,nan(x)))
+        end
+    end
+end
+    
+#functions with domain in -1 <= x <= 1
+#the value is set to zero if not it domain
+for f in (:asin,:asind,
+    :acos,:acosd,
+    )
+    @eval begin
+        $f(x) = Base.$f(x)
+        function $f(x::Real)
+            in_domain = abs(x) <= 1
+            Base.$f(ifelse(in_domain,x,nan(x)))
+        end
+    end
+end
+
+for f in (:asec,:asecd,
+    :acsc,:acscd,
+    )
+    @eval begin
+        $f(x) = Base.$f(x)
+        function $f(x::Real)
+            in_domain = !iszero(x)
+            Base.$f(ifelse(in_domain,x,nan(x)))
+        end
+    end
+end
+
+# Don't override built-in ^ operator
+function pow(x::Real, y::Real)
+    z = ifelse(x>=zero(x),x,nan(x))
+    return z^y
+end 
+
+pow(x,y) = x^y
+
+"""
+NativeNaNMath.sum(A)
+##### Args:
+* `A`: An array of floating point numbers
+##### Returns:
+*    Returns the sum of all elements in the array, ignoring NaN's.
+##### Examples:
+```julia
+using NativeNaNMath
+NativeNaNMath.sum([1., 2., NaN]) # result: 3.0
+```
+"""
+function sum(x::AbstractArray{T}) where T<:AbstractFloat
+    if length(x) == 0
+        result = zero(eltype(x))
+    else
+        result = convert(eltype(x), NaN)
+        for i in x
+            if !isnan(i)
+                if isnan(result)
+                    result = i
+                else
+                    result += i
+                end
+            end
+        end
+    end
+
+    if isnan(result)
+        @warn "All elements of the array, passed to \"sum\" are NaN!"
+    end
+    return result
+end
+
+"""
+NativeNaNMath.median(A)
+##### Args:
+* `A`: An array of floating point numbers
+##### Returns:
+*   Returns the median of all elements in the array, ignoring NaN's.
+    Returns NaN for an empty array or array containing NaNs only.
+##### Examples:
+```jldoctest
+julia> using NativeNaNMath
+julia> NativeNaNMath.median([1., 2., 3., NaN])
+2.
+julia> NativeNaNMath.median([1., 2., NaN])
+1.5
+julia> NativeNaNMath.median([NaN])
+NaN
+```
+"""
+median(x::AbstractArray{<:AbstractFloat}) = median(collect(Iterators.flatten(x)))
+
+function median(x::AbstractVector{<:AbstractFloat})
+
+    x = sort(filter(!isnan, x))
+
+    n = length(x)
+    if n == 0
+        return convert(eltype(x), NaN)
+    elseif isodd(n)
+        ind = ceil(Int, n/2)
+        return x[ind]
+    else
+        ind = Int(n/2)
+        lower = x[ind]
+        upper = x[ind+1]
+        return (lower + upper) / 2
+    end
+
+end
+
+"""
+NativeNaNMath.maximum(A)
+##### Args:
+* `A`: An array of floating point numbers
+##### Returns:
+*    Returns the maximum of all elements in the array, ignoring NaN's.
+##### Examples:
+```julia
+using NativeNaNMath
+NativeNaNMath.maximum([1., 2., NaN]) # result: 2.0
+```
+"""
+function maximum(x::AbstractArray{T}) where T<:AbstractFloat
+    result = convert(eltype(x), NaN)
+    for i in x
+        if !isnan(i)
+            if (isnan(result) || i > result)
+                result = i
+            end
+        end
+    end
+    return result
+end
+
+"""
+NativeNaNMath.minimum(A)
+##### Args:
+* `A`: An array of floating point numbers
+##### Returns:
+*    Returns the minimum of all elements in the array, ignoring NaN's.
+##### Examples:
+```julia
+using NativeNaNMath
+NativeNaNMath.minimum([1., 2., NaN]) # result: 1.0
+```
+"""
+function minimum(x::AbstractArray{T}) where T<:AbstractFloat
+    result = convert(eltype(x), NaN)
+    for i in x
+        if !isnan(i)
+            if (isnan(result) || i < result)
+                result = i
+            end
+        end
+    end
+    return result
+end
+
+"""
+NativeNaNMath.extrema(A)
+##### Args:
+* `A`: An array of floating point numbers
+##### Returns:
+*    Returns the minimum and maximum of all elements in the array, ignoring NaN's.
+##### Examples:
+```julia
+using NativeNaNMath
+NativeNaNMath.extrema([1., 2., NaN]) # result: 1.0, 2.0
+```
+"""
+function extrema(x::AbstractArray{T}) where T<:AbstractFloat
+    resultmin, resultmax = convert(eltype(x), NaN), convert(eltype(x), NaN)
+    for i in x
+        if !isnan(i)
+            if (isnan(resultmin) || i < resultmin)
+                resultmin = i
+            end
+            if (isnan(resultmax) || i > resultmax)
+                resultmax = i
+            end
+        end
+    end
+    return resultmin, resultmax
+end
+
+"""
+NativeNaNMath.mean(A)
+##### Args:
+* `A`: An array of floating point numbers
+##### Returns:
+*    Returns the arithmetic mean of all elements in the array, ignoring NaN's.
+##### Examples:
+```julia
+using NativeNaNMath
+NativeNaNMath.mean([1., 2., NaN]) # result: 1.5
+```
+"""
+function mean(x::AbstractArray{T}) where T<:AbstractFloat
+    return mean_count(x)[1]
+end
+
+"""
+Returns a tuple of the arithmetic mean of all elements in the array, ignoring NaN's,
+and the number of non-NaN values in the array.
+"""
+function mean_count(x::AbstractArray{T}) where T<:AbstractFloat
+    z = zero(eltype(x))
+    sum = z
+    count = 0
+    @simd for i in x
+        count += ifelse(isnan(i), 0, 1)
+        sum += ifelse(isnan(i), z, i)
+    end
+    result = sum / count
+    return (result, count)
+end
+
+"""
+NativeNaNMath.var(A)
+##### Args:
+* `A`: A one dimensional array of floating point numbers
+##### Returns:
+* Returns the sample variance of a vector A. The algorithm will return
+  an estimator of the  generative distribution's variance under the
+  assumption that each entry of v is an IID drawn from that generative
+  distribution. This computation is  equivalent to calculating \\
+  sum((v - mean(v)).^2) / (length(v) - 1). NaN values are ignored.
+##### Examples:
+```julia
+using NativeNaNMath
+NativeNaNMath.var([1., 2., NaN]) # result: 0.5
+```
+"""
+function var(x::Vector{T}) where T<:AbstractFloat
+    mean_val, n = mean_count(x)
+    if !isnan(mean_val)
+        sum_square = zero(eltype(x))
+        for i in x
+            if !isnan(i)
+                sum_square += (i - mean_val)^2
+            end
+        end
+        return sum_square / (n - one(eltype(x)))
+    else
+        return mean_val # NaN or NaN32
+    end
+end
+
+"""
+NativeNaNMath.std(A)
+##### Args:
+* `A`: A one dimensional array of floating point numbers
+##### Returns:
+* Returns the standard deviation of a vector A. The algorithm will return
+  an estimator of the  generative distribution's standard deviation under the
+  assumption that each entry of v is an IID drawn from that generative
+  distribution. This computation is  equivalent to calculating \\
+  sqrt(sum((v - mean(v)).^2) / (length(v) - 1)). NaN values are ignored.
+##### Examples:
+```julia
+using NativeNaNMath
+NativeNaNMath.std([1., 2., NaN]) # result: 0.7071067811865476
+```
+"""
+function std(x::Vector{T}) where T<:AbstractFloat
+    return sqrt(var(x))
+end
+
+"""
+    NativeNaNMath.min(x, y)
+Compute the IEEE 754-2008 compliant minimum of `x` and `y`. As of version 0.6 of Julia,
+`Base.min(x, y)` will return `NaN` if `x` or `y` is `NaN`. `NativeNaNMath.min` favors values over
+`NaN`, and will return whichever `x` or `y` is not `NaN` in that case.
+## Examples
+```julia
+julia> NativeNaNMath.min(NaN, 0.0)
+0.0
+julia> NativeNaNMath.min(1, 2)
+1
+```
+"""
+min(x::T, y::T) where {T<:AbstractFloat} = ifelse((y < x) | (signbit(y) > signbit(x)),
+                                           ifelse(isnan(y), x, y),
+                                           ifelse(isnan(x), y, x))
+
+"""
+    NativeNaNMath.max(x, y)
+Compute the IEEE 754-2008 compliant maximum of `x` and `y`. As of version 0.6 of Julia,
+`Base.max(x, y)` will return `NaN` if `x` or `y` is `NaN`. `NativeNaNMath.max` favors values over
+`NaN`, and will return whichever `x` or `y` is not `NaN` in that case.
+## Examples
+```julia
+julia> NativeNaNMath.max(NaN, 0.0)
+0.0
+julia> NativeNaNMath.max(1, 2)
+2
+```
+"""
+max(x::T, y::T) where {T<:AbstractFloat} = ifelse((y > x) | (signbit(y) < signbit(x)),
+                                           ifelse(isnan(y), x, y),
+                                           ifelse(isnan(x), y, x))
+
+min(x::Real, y::Real) = min(promote(x, y)...)
+max(x::Real, y::Real) = max(promote(x, y)...)
+
+function min(x::BigFloat, y::BigFloat)
+    isnan(x) && return y
+    isnan(y) && return x
+    return Base.min(x, y)
+end
+
+function max(x::BigFloat, y::BigFloat)
+    isnan(x) && return y
+    isnan(y) && return x
+    return Base.max(x, y)
+end
+
+# Integers can't represent NaN
+min(x::Integer, y::Integer) = Base.min(x, y)
+max(x::Integer, y::Integer) = Base.max(x, y)
+
+min(x::Real) = x
+max(x::Real) = x
+
+# Multi-arg versions
+for f in (:min, :max)
+    @eval ($f)(a, b, c, xs...) = Base.afoldl($f, ($f)(($f)(a, b), c), xs...)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,86 @@
 using NativeNaNMath
 using Test
 
-@testset "NativeNaNMath.jl" begin
-    # Write your tests here.
+@testset "log" begin
+    @test isnan(NativeNaNMath.log(-10))
+    @test isnan(NativeNaNMath.log1p(-100))
+end
+
+@testset "pow" begin
+    @test isnan(NativeNaNMath.pow(-1.5,2.3))
+    @test isnan(NativeNaNMath.pow(-1.5f0,2.3f0))
+    @test isnan(NativeNaNMath.pow(-1.5,2.3f0))
+    @test isnan(NativeNaNMath.pow(-1.5f0,2.3))
+    @test NativeNaNMath.pow(-1,2) isa Float64
+    @test NativeNaNMath.pow(-1.5f0,2) isa Float32
+    @test NativeNaNMath.pow(-1.5f0,2//1) isa Float32
+    @test NativeNaNMath.pow(-1.5f0,2.3f0) isa Float32
+    @test NativeNaNMath.pow(-1.5f0,2.3) isa Float64
+    @test NativeNaNMath.pow(-1.5,2) isa Float64
+    @test NativeNaNMath.pow(-1.5,2//1) isa Float64
+    @test NativeNaNMath.pow(-1.5,2.3f0) isa Float64
+    @test NativeNaNMath.pow(-1.5,2.3) isa Float64
+end
+
+@testset "sqrt" begin
+    @test isnan(NativeNaNMath.sqrt(-5))
+    @test NativeNaNMath.sqrt(5) == Base.sqrt(5)
+end
+
+@testset "reductions" begin
+    @test NativeNaNMath.sum([1., 2., NaN]) == 3.0
+    @test NativeNaNMath.sum([1. 2.; NaN 1.]) == 4.0
+    @test isnan(NativeNaNMath.sum([NaN, NaN]))
+    @test NativeNaNMath.sum(Float64[]) == 0.0
+    @test NativeNaNMath.sum([1f0, 2f0, NaN32]) === 3.0f0
+    @test NativeNaNMath.maximum([1., 2., NaN]) == 2.0
+    @test NativeNaNMath.maximum([1. 2.; NaN 1.]) == 2.0
+    @test NativeNaNMath.minimum([1., 2., NaN]) == 1.0
+    @test NativeNaNMath.minimum([1. 2.; NaN 1.]) == 1.0
+    @test NativeNaNMath.extrema([1., 2., NaN]) == (1.0, 2.0)
+    @test NativeNaNMath.extrema([2., 1., NaN]) == (1.0, 2.0)
+    @test NativeNaNMath.extrema([1. 2.; NaN 1.]) == (1.0, 2.0)
+    @test NativeNaNMath.extrema([2. 1.; 1. NaN]) == (1.0, 2.0)
+    @test NativeNaNMath.extrema([NaN, -1., NaN]) == (-1.0, -1.0)
+end
+
+@testset "statistics" begin
+    @test NativeNaNMath.mean([1., 2., NaN]) == 1.5
+    @test NativeNaNMath.mean([1. 2.; NaN 3.]) == 2.0
+    @test NativeNaNMath.var([1., 2., NaN]) == 0.5
+    @test NativeNaNMath.std([1., 2., NaN]) == 0.7071067811865476
+    @test NativeNaNMath.median([1.]) == 1.
+    @test NativeNaNMath.median([1., NaN]) == 1.
+    @test NativeNaNMath.median([NaN, 1., 3.]) == 2.
+    @test NativeNaNMath.median([1., 3., 2., NaN]) == 2.
+    @test NativeNaNMath.median([NaN, 1, 3]) == 2.
+    @test NativeNaNMath.median([1, 2, NaN]) == 1.5
+    @test NativeNaNMath.median([1 2; NaN NaN]) == 1.5
+    @test NativeNaNMath.median([NaN 2; 1 NaN]) == 1.5
+    @test isnan(NativeNaNMath.median(Float64[]))
+    @test isnan(NativeNaNMath.median(Float32[]))
+    @test isnan(NativeNaNMath.median([NaN]))
+end
+
+@testset "min/max" begin
+    @test NativeNaNMath.min(1, 2) == 1
+    @test NativeNaNMath.min(1.0, 2.0) == 1.0
+    @test NativeNaNMath.min(1, 2.0) == 1.0
+    @test NativeNaNMath.min(BigFloat(1.0), 2.0) == BigFloat(1.0)
+    @test NativeNaNMath.min(BigFloat(1.0), BigFloat(2.0)) == BigFloat(1.0)
+    @test NativeNaNMath.min(NaN, 1) == 1.0
+    @test NativeNaNMath.min(NaN32, 1) == 1.0f0
+    @test isnan(NativeNaNMath.min(NaN, NaN))
+    @test isnan(NativeNaNMath.min(NaN))
+    @test NaNMath.min(NaN, NaN, 0.0, 1.0) == 0.0
+    @test NativeNaNMath.max(1, 2) == 2
+    @test NativeNaNMath.max(1.0, 2.0) == 2.0
+    @test NativeNaNMath.max(1, 2.0) == 2.0
+    @test NativeNaNMath.max(BigFloat(1.0), 2.0) == BigFloat(2.0)
+    @test NativeNaNMath.max(BigFloat(1.0), BigFloat(2.0)) == BigFloat(2.0)
+    @test NativeNaNMath.max(NaN, 1) == 1.0
+    @test NativeNaNMath.max(NaN32, 1) == 1.0f0
+    @test isnan(NativeNaNMath.max(NaN, NaN))
+    @test isnan(NativeNaNMath.max(NaN))
+    @test NativeNaNMath.max(NaN, NaN, 0.0, 1.0) == 1.0
 end
